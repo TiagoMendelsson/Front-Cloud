890a7ae5a54ab72d36ffc86a58815f89ee4e7eb4
TiagoMendelsson <tiago.passos@indt.org.br>
2023-02-13 11:10:56 -0400

    tudo comentado

--- src/app/home-tiago/tiagopage/tiagopage.component.ts
+++ src/app/home-tiago/tiagopage/tiagopage.component.ts
@@ -13,107 +13,107 @@ import { startWith } from 'rxjs';
 export class TiagopageComponent {
  canvas:any
  ctx:any
-  
- constructor(private el:ElementRef){
 }
-  ngAfterViewInit() {
+//  constructor(private el:ElementRef){
+// }
+//   ngAfterViewInit() {
     
-  }
-  ngOnInit() {
-    this.canvas=this.el.nativeElement.querySelector('canvas')
-    this.ctx = this.canvas.getContext('2d');
-    const image = new Image()
-    image.src = 'assets/bokunohero.jpg'
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
-    }
+//   }
+//   ngOnInit() {}
+  //   this.canvas=this.el.nativeElement.querySelector('canvas')
+  //   this.ctx = this.canvas.getContext('2d');
+  //   const image = new Image()
+  //   image.src = 'assets/bokunohero.jpg'
+  //   image.onload = () => {
+  //     this.ctx.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
+  //   }
 
-    const canvas = document.getElementById("canvas")
+  //   const canvas = document.getElementById("canvas")
 
-    this.canvas.width = window.innerWidth - 60;
-    this.canvas.height = 400;
+  //   this.canvas.width = window.innerWidth - 60;
+  //   this.canvas.height = 400;
     
     
-    let context = this.canvas.getContext("2d")
-    let start_background_color = "white";
-    context.fillStyle = start_background_color;
-    context.fillRect(0,0, Canvas.width, Canvas.height);
+  //   let context = this.canvas.getContext("2d")
+  //   let start_background_color = "white";
+  //   context.fillStyle = start_background_color;
+  //   context.fillRect(0,0, Canvas.width, Canvas.height);
 
-    let draw_color = "black";
-    let draw_width = "2";
-    let is_drawing = false;
+  //   let draw_color = "black";
+  //   let draw_width = "2";
+  //   let is_drawing = false;
     
-    let restore_array: any[]= [];
-    let index = 1;
+  //   let restore_array: any[]= [];
+  //   let index = 1;
     
-    function change_color(element: any) {
-      draw_color = element.style.background;
-    }
+  //   function change_color(element: any) {
+  //     draw_color = element.style.background;
+  //   }
 
-    this.canvas.addEventListener("touchstart", start, false);
-    this.canvas.addEventListener("touchmove", draw, false);
-    this.canvas.addEventListener("touchdown", start, false);
-    this.canvas.addEventListener("touchmove", draw, false);
+  //   this.canvas.addEventListener("touchstart", start, false);
+  //   this.canvas.addEventListener("touchmove", draw, false);
+  //   this.canvas.addEventListener("touchdown", start, false);
+  //   this.canvas.addEventListener("touchmove", draw, false);
 
-    function start(event: any) {
-      is_drawing = true;
+  //   function start(event: any) {
+  //     is_drawing = true;
 
-      context.beginPath();
-      context.MoveTo( event.clientX - Canvas.offsetLeft,
-                       event.clientY - Canvas.offsetTop);
+  //     context.beginPath();
+  //     context.MoveTo( event.clientX - Canvas.offsetLeft,
+  //                      event.clientY - Canvas.offsetTop);
       
-      event.preventDefault();
-    }
+  //     event.preventDefault();
+  //   }
 
-    function draw(event: any) {
-      if (  is_drawing  ) {
-        context.LineTo(event.clientX - Canvas.offsetLeft,
-                      event.clientY - Canvas.offsetTop);
+  //   function draw(event: any) {
+  //     if (  is_drawing  ) {
+  //       context.LineTo(event.clientX - Canvas.offsetLeft,
+  //                     event.clientY - Canvas.offsetTop);
 
-        context.strokeStyle =draw_color;
-        context.lineWidth = draw_width;
-        context.lineCap = "round";
-        context.lineJoin = "round";
-        context.stroke ();
-      }
+  //       context.strokeStyle =draw_color;
+  //       context.lineWidth = draw_width;
+  //       context.lineCap = "round";
+  //       context.lineJoin = "round";
+  //       context.stroke ();
+  //     }
       
-    }
-    function stop(event:any) {
-      if (is_drawing) {
-        context.stroke()
-        context.closePath();
-        is_drawing = false;
-      }
-      event.preventDefault();
-      if (event.type != 'mouseout') {
-        restore_array.push(context.getImageData(0,0, Canvas.width, Canvas.height));
-        index += 1;
+  //   }
+  //   function stop(event:any) {
+  //     if (is_drawing) {
+  //       context.stroke()
+  //       context.closePath();
+  //       is_drawing = false;
+  //     }
+  //     event.preventDefault();
+  //     if (event.type != 'mouseout') {
+  //       restore_array.push(context.getImageData(0,0, Canvas.width, Canvas.height));
+  //       index += 1;
       
-      }
+  //     }
       
-    }
+  //   }
 
-    function clear_canvas() {
-      context.fillStyle = start_background_color;
-      context.clearRect(0,0, Canvas.width, Canvas.height);
-      context.fillRect(0,0, Canvas.width, Canvas.height);
+  //   function clear_canvas() {
+  //     context.fillStyle = start_background_color;
+  //     context.clearRect(0,0, Canvas.width, Canvas.height);
+  //     context.fillRect(0,0, Canvas.width, Canvas.height);
       
-      restore_array = [];
-      index= -1;
-    }
+  //     restore_array = [];
+  //     index= -1;
+  //   }
 
-    function undo_last () {
-      if (index <= 0) {
-        clear_canvas();
-      }else {
-        index -= 1;
-        restore_array.pop();
-        context.putImageData(restore_array[index], 0, 0);
-      }
-    }
+  //   function undo_last () {
+  //     if (index <= 0) {
+  //       clear_canvas();
+  //     }else {
+  //       index -= 1;
+  //       restore_array.pop();
+  //       context.putImageData(restore_array[index], 0, 0);
+  //     }
+  //   }
 
-  }
+  // }
   
 
   
-}
+
